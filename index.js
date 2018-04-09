6df5ddfa1b4c0d6544fe2a90c11d44be74b3b32b
Susanna Kosonen <kosonen.susanna@gmail.com>
2018-04-09 20:16:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,10 +28,11 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newArray = kittens.slice(1);
+  var newArray = kittens.slice(0, kittens.length - 1);
   return newArray;
 }
 
 function removeFirstKitten() {
-  
+  var newArray = kittens.slice(1);
+  return newArray;
 }
