8c2a3bad11ec92bc9f4b1d266cf2ef2aa54ae2d0
Susanna Kosonen <kosonen.susanna@gmail.com>
2018-04-09 20:15:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(0, 1);
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten() {
+}
+
+function removeFirstKitten() {
+  
+}
